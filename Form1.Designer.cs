1981e8451113a544e962ed0a220260802f47ebb6
craftxbox <lukie465@hotmail.com>
2017-09-09 16:34:54 -0230

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(702, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DESTROY IMAGE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(391, 386);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            this.pictureBox1.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.pictureBox1_LoadCompleted);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(409, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(384, 386);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(720, 404);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 38);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 454);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Image Destroyer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.PictureBox pictureBox2;
+        public System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        delegate void SetTextCallback(string text);
+        delegate void SetImageCallback(Image img);
+
+        string filePath;
+        Random random = new Random();
+        private static Bitmap destroyedImage;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "JPEG Image files|*.jpg|PNG Image files|*.png|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        pictureBox1.Image =  Image.FromStream(myStream);
+                        filePath = openFileDialog1.FileName;
+                        openFileDialog1.Dispose();
+                        myStream.Dispose();
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Are you sure you want to destroy this?\nThis action is PERMANENT!",
+                                     "Destruction Confirmation",
+                                     MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                try
+                {
+                    destroyedImage = (Bitmap)pictureBox1.Image;
+                    int fuckingWidth = pictureBox1.Image.Width;
+                    int fuckingHeight = pictureBox1.Image.Height;
+                    foreach (int y in Enumerable.Range(0, fuckingHeight))
+                    {
+                        foreach (int x in Enumerable.Range(0, fuckingWidth))
+                        {
+
+                            Color c = destroyedImage.GetPixel(x, y);
+                            c = setRandomColor(c);
+                            destroyedImage.SetPixel(x, y, c);
+                            this.SetText(x + ", " + y);
+
+
+                        }
+                        pictureBox2.Image = destroyedImage;
+                        this.Update();
+
+                    }
+                    this.SetText("DONE");
+                    var confirmResult2 = MessageBox.Show("This is your LAST CHANCE! Press \"No\" to cancel overwriting the file!",
+                         "Final Destruction Confirmation",
+                         MessageBoxButtons.YesNo);
+                    if (confirmResult2 == DialogResult.Yes)
+                    {
+                        Image out1 = (Image)destroyedImage;
+                        Bitmap output = (Bitmap)out1;
+                        if (filePath.EndsWith(".jpg")){
+                            output.Save(filePath, ImageFormat.Jpeg);
+                        } else
+                        {
+                            output.Save(filePath, ImageFormat.Png);
+                        }
+                       
+                        
+                    }
+                    
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message + "\n" + ex.StackTrace);
+                }
+            }
+            
+            
+
+        }
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.button1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.button1.Text = text;
+            }
+        }
+        private void SetAltPic(Image img)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.pictureBox2.InvokeRequired)
+            {
+                SetImageCallback d = new SetImageCallback(SetAltPic);
+                this.Invoke(d, new object[] { img });
+            }
+            else
+            {
+                this.pictureBox2.Image = img;
+            }
+        }
+
+        private int getRandom8BitNumber()
+        {
+            
+            int rand = random.Next(255);
+            return rand;
+        }
+        private Color setRandomColor(Color c)
+        {
+            c = Color.FromArgb(getRandom8BitNumber(), getRandom8BitNumber(), getRandom8BitNumber());
+            return c;
+        }
+
+        private void pictureBox1_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            pictureBox1.Image = new Bitmap(ImageDestroyer.Properties.Resources.image);
+        }
+    }
+}
